5c46d41d8b6addd7e0a9714539b789cdb4243774
SeetKokPeng <90586151+SeetKokPeng@users.noreply.github.com>
2021-09-13 10:45:21 +0800

    Add files via upload

---
+++ Lab 2Q1.py
@@ -0,0 +1,6 @@
+# Student ID:1001030912 #
+# Student Name: Suhaimi Sarip #
+
+kilo = float (input ("Please enter the value of kilogram"))
+gram = kilo * 1000
+print("For {:.2f} kilogram is equivalent to {:.2f}gram ".format(kilo,gram))

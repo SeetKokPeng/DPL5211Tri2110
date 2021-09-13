81d6c6a2c3409ee50fcd88331c231c2724a57891
SeetKokPeng <90586151+SeetKokPeng@users.noreply.github.com>
2021-09-13 11:05:50 +0800

    Add files via upload

---
+++ Lab 1 Q2.py
@@ -0,0 +1,13 @@
+
+# Student ID: 1201200333 #
+# Student Name : Seet Kok Peng #
+
+number_1 = int(input("Please enter number 1: "))
+number_2 = int(input("Please enter number 2: "))
+
+total = number_1+number_2
+
+# print("The total number is ", total)
+# The sum of 5 and 6 is 11
+
+print("The Sum is ",total," ",number_1,"and  ",number_2)
